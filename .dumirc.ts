4ae09a821e98bc5fa012db736a8185a5bd405d92
weiji <weiji@qunhemail.com>
2023-02-10 19:23:31 +0800

    doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'dumi';
 
 
 
+
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {},
   base: '/dumi-pro',
-  publicPath: '/dumi-pro',
+  publicPath: '/dumi-pro/',
 });
